52afda7b86f9b197fc8dd530619a1e2d1eedd964
6410615147 <6410615147@student.tu.ac.th>
2024-04-03 09:47:54 +0700

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Babypowder on 3/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
